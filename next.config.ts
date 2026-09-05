4ca4a1275f2a2ae1b7f97964afd3ae7e53b0607f
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2026-09-06 01:19:16 +0800

    Hide dev indicators

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/duolingo-japanese-vocabulary",
   allowedDevOrigins: ["host.docker.internal"],
+  devIndicators: false,
 };
 
 export default nextConfig;
