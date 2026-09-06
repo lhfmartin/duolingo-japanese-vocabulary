c11fecbdc6d469983b42b2e37017a3e666f3bf11
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2026-09-06 11:50:06 +0800

    Revert "Hide dev indicators"
    This reverts commit 4ca4a1275f2a2ae1b7f97964afd3ae7e53b0607f.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/duolingo-japanese-vocabulary",
   allowedDevOrigins: ["host.docker.internal"],
-  devIndicators: false,
 };
 
 export default nextConfig;
