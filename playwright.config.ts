26aa5878e2ca045b0dfbbb768a135a636ce13b71
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2026-09-16 00:07:06 +0800

    Set toHaveScreenshot and toMatchSnapshot threshold to 0.05

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,10 @@ export default defineConfig({
   use: { viewport: { width: 1920, height: 1080 } },
   expect: {
     toHaveScreenshot: {
-      threshold: 0,
+      threshold: 0.05,
     },
     toMatchSnapshot: {
-      threshold: 0,
+      threshold: 0.05,
     },
   },
 });
